4179fd8dd50b21b9f0a8f09a4346778fe7d1c5c6
Shigeru Fujiwara <sgr0502@gmail.com>
2010-12-06 02:40:25 +0900

    changed version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nicoalert "1.0.0-SNAPSHOT"
+(defproject nicoalert "1.0.0-beta"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
