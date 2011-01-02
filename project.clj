57b3a2ecf27aca6bd2c2c193df6e967430997492
sgr <sgr@mini12.(none)>
2011-01-02 19:39:12 +0900

    updated swank-clojure.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [org.swinglabs/swingx-core "1.6.2-2"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
   :omit-source true
   :resources-path "resource"
   :jar-exclusions [#"(?:^|/).svn" #"(?:^|/).git" #"(?:\w+).xcf$" #"(?:\w+).pdn$"]
