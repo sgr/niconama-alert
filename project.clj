210b3164fb7c39f4598db09c5934d71c36d50f9a
Shigeru Fujiwara <sgr0502@gmail.com>
2011-01-19 16:52:41 +0900

    Release Candidate 6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nicoalert "1.0.0-rc5"
+(defproject nicoalert "1.0.0-rc6"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
