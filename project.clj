452789ef828699e5384e9f1887a70579a52ce5b6
Shigeru Fujiwara <sgr0502@gmail.com>
2011-10-10 00:02:47 +0900

    Release Candidate 11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nicoalert "1.0.0-rc10"
+(defproject nicoalert "1.0.0-rc11"
   :description "Niconama Alert J"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
