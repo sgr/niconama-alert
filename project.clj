4a320e009af5f68b26f8d0a9e8f9030baefb1daa
Shigeru Fujiwara <sgr0502@gmail.com>
2011-11-14 03:13:55 +0900

    1.0.0 has been released.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nicoalert "1.0.0-rc12"
+(defproject nicoalert "1.0.0"
   :description "Niconama Alert J"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
