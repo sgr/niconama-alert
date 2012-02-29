dab63b5c212d05482abef94d39af84e849ec65e3
Shigeru Fujiwara <sgr0502@gmail.com>
2012-02-29 20:51:18 +0900

    update dev-dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [enlive "1.0.0"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]]
   :omit-source true
   :resources-path "resource"
   :jar-exclusions [#"(?:^|/).svn" #"(?:^|/).git" #"(?:\w+).xcf$" #"(?:\w+).pdn$"]
