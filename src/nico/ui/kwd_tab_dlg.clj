f7806057abbcd8db7462ce237d6913b2e3ee712b
Shigeru Fujiwara <sgr0502@gmail.com>
2012-06-17 13:35:21 +0900

    divided kwd_tab_dlg into query-panel, target-panel, kwd-tab-dlg

--- src/nico/ui/kwd_tab_dlg.clj
+++ src/nico/ui/kwd_tab_dlg.clj
@@ -4,10 +4,11 @@
   nico.ui.kwd-tab-dlg
   (:use [clojure.tools.swing-utils :only [do-swing add-action-listener]]
 	[clojure.tools.logging])
-  (:require [nico.ui.util :as uu])
-  (:import [java.awt BorderLayout Color FlowLayout Dimension]
-	   [javax.swing BorderFactory GroupLayout SpringLayout
-			JButton JCheckBox JDialog JLabel JPanel JScrollPane JTextArea JTextField]
+  (:require [nico.ui.query-panel :as uq]
+            [nico.ui.target-panel :as ut]
+            [nico.ui.util :as uu])
+  (:import [java.awt Color FlowLayout Dimension]
+	   [javax.swing BorderFactory SpringLayout JDialog JLabel JPanel JTextField]
 	   [javax.swing.event DocumentListener]
 	   [javax.swing.text PlainDocument]))
 
@@ -16,105 +17,33 @@
 (def ^{:private true} QUERY-PANEL-SIZE (Dimension. 440 130))
 (def ^{:private true} BTN-PANEL-SIZE (Dimension. 440 30))
 
-(defn transq
-  "translate query string into function sexp.
-   It returns nil when malformed query string.
-   If you call translated query function, eval it.
-  \"word\" -> (fn [s] (re-find #\"(?i)word\" s))
-  (and \"a\" (not \"b\")) -> (fn [s] (and (re-find #\"(?i)a\") (not (re-find #\"(?i)b\")))) "
-  [q]
-  (letfn [(trans-item
-	   [o]
-	   (cond (list? o) (if (< 1 (count o))
-			     (let [op (first o)]
-			       (if (or (= 'and op) (= 'or op) (= 'not op))
-				 (cons op (map trans-item (rest o)))
-				 (throw (IllegalArgumentException.
-					 (format "Invalid operator: %s" op)))))
-			     (throw (IllegalArgumentException.
-				     (format "Almost empty list was given: %s" (pr-str o)))))
-		 (string? o) (if-not (empty? o)
-			       (list 're-find (list 're-pattern (str "(?i)" o)) 's)
-			       (throw (IllegalArgumentException. "Empty string was given.")))
-		 (symbol? o) (list 're-find (list 're-pattern (str "(?i)" (str o))) 's)
-		 :else (list 're-find (list 're-pattern (str "(?i)" (str o))) 's)))
-	  (transq-aux
-	   [q]
-	   (with-open [r (java.io.PushbackReader. (java.io.StringReader. q))]
-	     (loop [o (read r nil :EOF), lst '()]
-	       (if (= :EOF o) lst
-		   (recur (read r nil :EOF) (conj lst (trans-item o)))))))]
-    (let [rr (transq-aux q)]
-      (condp = (count rr)
-	  0 (throw (IllegalArgumentException. "Empty query."))
-	  1 (list 'fn '[s] (first rr))
-	  (list 'fn '[s] (conj rr 'and))))))
-
 (defn keyword-tab-dialog
   [parent title pref ok-fn]
   (let [dlg (JDialog. parent title true)
 	title-panel (JPanel.), title-field (JTextField.), title-doc (PlainDocument.)
 	title-border (.getBorder title-field)
-	query-panel (JPanel.), query-area (JTextArea.), query-doc (PlainDocument.)
-	query-border (.getBorder query-area)
-	target-outer-panel (JPanel.)
-	target-panel (doto (JPanel.) (.setBorder (BorderFactory/createEmptyBorder 1 1 1 1)))
-	target-border (.getBorder target-panel)
-	cb-title (JCheckBox. "タイトル"), cb-desc (JCheckBox. "説明"), cb-owner (JCheckBox. "放送主")
-	cb-category (JCheckBox. "カテゴリ"), cb-comm-name (JCheckBox. "コミュ名")
+        query-panel (nico.ui.QueryPanel. (:query pref))
+        target-panel (nico.ui.TargetPanel. (:target pref))
 	btn-panel (JPanel.), btn-ok (uu/btn "OK")
 	p (.getLocationOnScreen parent)]
     (letfn [(read-title [] (.getText title-doc 0 (.getLength title-doc)))
-	    (read-query [] (.getText query-doc 0 (.getLength query-doc)))
 	    (check []
-		   (let [selected (or (.isSelected cb-title)
-				      (.isSelected cb-desc)
-				      (.isSelected cb-owner)
-				      (.isSelected cb-category)
-				      (.isSelected cb-comm-name))]
-		     (if (= 0 (.getLength title-doc))
-		       (.setBorder title-field (BorderFactory/createLineBorder Color/RED))
-		       (.setBorder title-field title-border))
-		     (if (= 0 (.getLength query-doc))
-		       (.setBorder query-area (BorderFactory/createLineBorder Color/RED))
-		       (.setBorder query-area query-border))
-		     (if (false? selected)
-		       (.setBorder target-panel (BorderFactory/createLineBorder Color/RED))
-		       (.setBorder target-panel target-border))
-		     (try
-                       (let [tq (transq (read-query))]
-                         (info (format "query: %s" tq))
-			 (eval tq)
-			 (.setBorder query-area query-border)
-			 (.setEnabled btn-ok true))
-		       (catch Exception e
-			 (warn (.getMessage e))
-			 (.setBorder query-area (BorderFactory/createLineBorder Color/RED))
-			 (.setEnabled btn-ok false)))
-		     (if (or (= 0 (.getLength title-doc))
-			     (= 0 (.getLength query-doc))
-			     (false? selected))
-		       (.setEnabled btn-ok false))))]
+              (if (= 0 (.getLength title-doc))
+                (.setBorder title-field (BorderFactory/createLineBorder Color/RED))
+                (.setBorder title-field title-border)))
+            (check-all []
+              (if (or (= 0 (.getLength title-doc))
+                      (not (.isOK query-panel))
+                      (not (.isOK target-panel)))
+                (.setEnabled btn-ok false)
+                (.setEnabled btn-ok true)))]
       (doto title-doc
 	(.addDocumentListener (proxy [DocumentListener] []
-				(changedUpdate [_] (check))
-				(insertUpdate [_] (check))
-				(removeUpdate [_] (check)))))
-      (doto query-doc
-	(.addDocumentListener (proxy [DocumentListener] []
-				(changedUpdate [_] (check))
-				(insertUpdate [_] (check))
-				(removeUpdate [_] (check)))))
-      (doseq [c [cb-title cb-desc cb-owner cb-category cb-comm-name]]
-	(doto c (add-action-listener (fn [_] (check)))))
-      (when-let [ts (:target pref)]
-	(when (seq? ts) (doseq [t ts] (condp = t
-					  :title (.setSelected cb-title true)
-					  :desc (.setSelected cb-desc true)
-					  :owner_name (.setSelected cb-owner true)
-					  :category (.setSelected cb-category true)
-					  :comm_name (.setSelected cb-comm-name true)
-					  nil))))
+				(changedUpdate [_] (check) (check-all))
+				(insertUpdate [_] (check) (check-all))
+				(removeUpdate [_] (check) (check-all)))))
+      (.addListener query-panel check-all)
+      (.addListener target-panel check-all)
       (let [title-label (JLabel. "タブタイトル"), layout (SpringLayout.)]
 	(doto title-field (.setDocument title-doc))
 	(when-let [t (:title pref)] (.setText title-field t))
@@ -129,33 +58,14 @@
 	(doto title-panel
 	  (.setPreferredSize TITLE-PANEL-SIZE)
 	  (.setLayout layout) (.add title-label) (.add title-field)))
-      (doto query-area
-	(.setLineWrap true)
-	(.setDocument query-doc))
-      (when-let [q (:query pref)] (.setText query-area q))
-      (doto query-panel
-	(.setBorder (BorderFactory/createTitledBorder "検索条件"))
-	(.setPreferredSize QUERY-PANEL-SIZE)
-	(uu/do-add-expand (JScrollPane. query-area) 5))
-      (doto target-panel
-	(.setLayout (FlowLayout.))
-	(.add cb-title) (.add cb-desc) (.add cb-owner) (.add cb-category) (.add cb-comm-name))
-      (doto target-outer-panel
-	(.setBorder (BorderFactory/createTitledBorder "検索対象"))
-	(.add target-panel))
       (doto btn-ok
 	(add-action-listener
 	 (fn [e] (do-swing
 		  (.setVisible dlg false)
 		  (ok-fn {:type :kwd
 			  :title (read-title)
-			  :query (read-query)
-			  :target (filter #(not (nil? %))
-					  (list (when (.isSelected cb-title) :title)
-					        (when (.isSelected cb-desc) :desc)
-					        (when (.isSelected cb-owner) :owner_name)
-						(when (.isSelected cb-category) :category)
-						(when (.isSelected cb-comm-name) :comm_name)))
+			  :query (.getQuery query-panel)
+			  :target (.getTargets target-panel)
 			  :alert (:alert pref)})
 		  (.dispose dlg)))))
       (.setDefaultButton (.getRootPane dlg) btn-ok)
@@ -177,17 +87,17 @@
 	  (.putConstraint SpringLayout/EAST title-panel -5 SpringLayout/EAST cpane)
 	  (.putConstraint SpringLayout/WEST query-panel 5 SpringLayout/WEST cpane)
 	  (.putConstraint SpringLayout/EAST query-panel -5 SpringLayout/EAST cpane)
-	  (.putConstraint SpringLayout/WEST target-outer-panel 5 SpringLayout/WEST cpane)
-	  (.putConstraint SpringLayout/EAST target-outer-panel -5 SpringLayout/EAST cpane)
+	  (.putConstraint SpringLayout/WEST target-panel 5 SpringLayout/WEST cpane)
+	  (.putConstraint SpringLayout/EAST target-panel -5 SpringLayout/EAST cpane)
 	  (.putConstraint SpringLayout/WEST btn-panel 5 SpringLayout/WEST cpane)
 	  (.putConstraint SpringLayout/EAST btn-panel -5 SpringLayout/EAST cpane)
 	  (.putConstraint SpringLayout/NORTH title-panel 5 SpringLayout/NORTH cpane)
 	  (.putConstraint SpringLayout/NORTH query-panel 5 SpringLayout/SOUTH title-panel)
-	  (.putConstraint SpringLayout/NORTH target-outer-panel 5 SpringLayout/SOUTH query-panel)
-	  (.putConstraint SpringLayout/NORTH btn-panel 5 SpringLayout/SOUTH target-outer-panel)
+	  (.putConstraint SpringLayout/NORTH target-panel 5 SpringLayout/SOUTH query-panel)
+	  (.putConstraint SpringLayout/NORTH btn-panel 5 SpringLayout/SOUTH target-panel)
 	  (.putConstraint SpringLayout/SOUTH btn-panel -5 SpringLayout/SOUTH cpane))
 	(doto cpane
-	  (.add title-panel) (.add query-panel) (.add target-outer-panel) (.add btn-panel)
+	  (.add title-panel) (.add query-panel) (.add target-panel) (.add btn-panel)
 	  (.setLayout layout)))
       (check)
       (doto dlg
@@ -195,4 +105,3 @@
 		      (+ (.y p) (int (/ (- (.getHeight parent) (.getHeight DLG-SIZE)) 2))))
 	(.setResizable false)
 	(.setMinimumSize DLG-SIZE)))))
-
---
+++ src/nico/ui/query_panel.clj
@@ -0,0 +1,71 @@
+;; -*- coding: utf-8-unix -*-
+(ns #^{:author "sgr"
+       :doc "query panel"}
+  nico.ui.query-panel
+  (:use [clojure.tools.logging])
+  (:require [query-utils :as qu]
+            [nico.ui.util :as uu])
+  (:import [java.awt Color Dimension]
+	   [javax.swing BorderFactory JPanel JScrollPane JTextArea JTextField]
+	   [javax.swing.event DocumentListener]
+	   [javax.swing.text PlainDocument]))
+
+(def ^{:private true} QUERY-PANEL-SIZE (Dimension. 440 130))
+
+(gen-class
+ :name nico.ui.QueryPanel
+ :extends javax.swing.JPanel
+ :prefix "qp-"
+ :constructors {[String] []}
+ :state state
+ :init init
+ :post-init post-init
+ :methods [[getQuery [] String]
+           [setQuery [String] void]
+           [addListener [clojure.lang.IFn] void]
+           [isOK [] boolean]])
+
+(defn- qp-init [query] [[] (atom {:ok false :rq nil :sq nil})])
+(defn- qp-getQuery [this] (when-let [rq (:rq @(.state this))] (rq)))
+(defn- qp-setQuery [this query] (when-let [sq (:sq @(.state this))] (sq query)))
+(defn- qp-isOK [this] (:ok @(.state this)))
+(defn- qp-addListener [this f] (when-let [al (:al @(.state this))] (al f)))
+
+(defn- qp-post-init [this query]
+  (let [query-area (JTextArea.), query-doc (PlainDocument.)
+	query-border (.getBorder query-area)]
+    (letfn [(set-ok [b]
+              (if b
+                (do (swap! (.state this) assoc :ok true)
+                    (.setBorder query-area query-border))
+                (do (swap! (.state this) assoc :ok false)
+                    (.setBorder query-area (BorderFactory/createLineBorder Color/RED)))))
+	    (check []
+              (if (< 0 (.getLength query-doc))
+                (try
+                  (let [tq (qu/to-where-clause (read-query) [:title])]
+                    (info (format "query: %s" tq))
+                    (set-ok true))
+                  (catch Exception e
+                    (warn (.getMessage e))
+                    (set-ok false)))
+                (set-ok false)))
+            (add-listener [f]
+              (.addDocumentListener query-doc (proxy [DocumentListener] []
+                                                (changedUpdate [_] (check) (f))
+                                                (insertUpdate [_] (check) (f))
+                                                (removeUpdate [_] (check) (f)))))
+            (read-query [] (.getText query-doc 0 (.getLength query-doc)))
+            (set-query [query] (.setText query-area query))]
+      (swap! (.state this) assoc :al add-listener)
+      (swap! (.state this) assoc :rq read-query)
+      (swap! (.state this) assoc :sq set-query)
+      (doto query-area
+	(.setLineWrap true)
+	(.setDocument query-doc))
+      (set-query query)
+      (check)
+      (doto this
+	(.setBorder (BorderFactory/createTitledBorder "検索条件"))
+	(.setPreferredSize QUERY-PANEL-SIZE)
+	(uu/do-add-expand (JScrollPane. query-area) 5)))))
