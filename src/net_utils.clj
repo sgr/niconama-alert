59f5bcc5e156b5122d057685bfdff908bb87f9e1
Shigeru Fujiwara <sgr0502@gmail.com>
2012-06-24 23:31:46 +0900

    made to retry fetching thumbnail when timeout is occurred

--- src/net_utils.clj
+++ src/net_utils.clj
@@ -2,10 +2,13 @@
 (ns #^{:author "sgr"
        :doc "Utility for networking."}
   net-utils
-  (:use [clojure.tools.logging]))
+  (:use [clojure.tools.logging])
+  (:import [java.util.concurrent TimeUnit]
+           [java.net SocketTimeoutException]))
 
 (def ^{:private true} CONNECT-TIMEOUT 5000)
 (def ^{:private true} READ-TIMEOUT    5000)
+(def ^{:private true} INTERVAL-RETRY  2000)
 
 (def HTTP-OPTS
   {:headers {"user-agent" "Niconama-alert J/1.1.0"}
@@ -23,10 +26,17 @@
            (error msg#)
            (throw (Exception. msg#)))))))
 
-(defn url-stream
-  ([^String url ^Integer connect-timeout ^Integer read-timeout]
-     (let [u (java.net.URL. url)]
-       (.getInputStream (doto (.openConnection u)
-			  (.setConnectTimeout connect-timeout)
-			  (.setReadTimeout read-timeout)))))
-  ([^String url] (url-stream url CONNECT-TIMEOUT READ-TIMEOUT)))
+(defn url-stream [^String url]
+  (letfn [(url-stream-aux [^String url ^Integer connect-timeout ^Integer read-timeout]
+            (try
+              (let [u (java.net.URL. url)]
+                (.getInputStream (doto (.openConnection u)
+                                   (.setConnectTimeout connect-timeout)
+                                   (.setReadTimeout read-timeout))))
+              (catch SocketTimeoutException e
+                (warn e (format "timeouted open input stream from: %s (%d, %d)" url connect-timeout read-timeout))
+                nil)))]
+    (if-let [is (url-stream-aux url CONNECT-TIMEOUT READ-TIMEOUT)]
+      is
+      (do (.sleep TimeUnit/SECONDS INTERVAL-RETRY)
+          (recur url)))))
