51d3b3c39337e4648c1b472895df03e029e2c685
Shigeru Fujiwara <sgr0502@gmail.com>
2012-07-12 23:04:44 +0900

    Released 1.5.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject nicoalert "1.5.0-rc4"
+(defproject nicoalert "1.5.0"
   :description "Niconama Alert J"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/math.numeric-tower "0.0.1"]
-                 [org.clojure/tools.logging "0.2.3"]
-                 [clj-http "0.4.3"]
+                 [org.clojure/tools.logging "0.2.4"]
+                 [clj-http "0.4.4"]
                  [swing-utils "0.2.0"]
 		 [enlive "1.0.1"]
                  [c3p0/c3p0 "0.9.1.2"]
