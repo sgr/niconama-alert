a8ca3b9eb59e72397c5c87857720d33df1e9ffd8
Shigeru Fujiwara <sgr0502@gmail.com>
2012-10-06 00:22:55 +0900

    Updated against lein2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nicoalert "1.5.1"
+(defproject nicoalert "1.5.2"
   :description "Niconama Alert J"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.1"]
@@ -7,12 +7,13 @@
                  [org.clojure/tools.logging "0.2.4"]
                  [clj-http "[0.5,)"]
                  [swing-utils "0.2.0"]
-		 [enlive "[1.0,)"]
-                 [com/mchange/c3p0 "[0.9,)"]
+                 [enlive "1.0.1"]
+                 [com.mchange/c3p0 "0.9.2-pre5"]
                  [com.h2database/h2 "[1.3,)"]]
-  :dev-dependencies [[swank-clojure "1.4.2"]]
+  :dev-dependencies [[org.clojure/tools.nrepl "0.2.0-beta9"]]
+;;  :dev-dependencies [[swank-clojure "1.4.2"]]
   :omit-source true
-  :resources-path "resource"
+;;  :resource-paths ["resources"]
   :jar-exclusions [#"(?:^|/).svn" #"(?:^|/).git" #"(?:\w+).xcf$" #"(?:\w+).pdn$"]
   :main nico.core)
 
