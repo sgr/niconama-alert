4fabb67089e706767ad57c12b6a5af6d8448913a
Shigeru Fujiwara <sgr0502@gmail.com>
2013-03-07 02:50:37 +0900

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject nicoalert "1.6.0"
   :description "Niconama Alert J"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/tools.logging "0.2.4"]
-                 [logutil "0.2.0"]
+                 [org.clojure/tools.logging "[0.2,)"]
+                 [logutil "[0.2,)"]
                  [clj-http "[0.6,)"]
                  [org.apache.httpcomponents/httpclient-cache "4.2.2"]
                  [swing-utils "0.2.0"]
                  [enlive "1.0.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [org.apache.commons/commons-lang3 "3.1"]
-                 [net.sourceforge.htmlcleaner/htmlcleaner "2.2"]]
+                 [net.sourceforge.htmlcleaner/htmlcleaner "2.2.1"]]
   :exclusions [org.apache.ant/ant]
   :debug false
   :profiles {:dev
