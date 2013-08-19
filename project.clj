5f77aa2e7f82a7c53694c1015a932f60d90eee7b
Shigeru Fujiwara <sgr0502@gmail.com>
2013-08-20 01:15:07 +0900

    ソース類の整理

--- project.clj
+++ project.clj
@@ -8,11 +8,12 @@
                  [logutil "[0.2,)"]
                  [clj-http "0.7.3"]
                  [org.apache.httpcomponents/httpclient-cache "4.2.5"]
+                 [input-parser "0.1.1"]
                  [swing-utils "0.2.0"]
                  [enlive "1.1.1"]
                  [org.xerial/sqlite-jdbc "3.7.15-M1"]
                  [org.apache.commons/commons-lang3 "3.1"]
-                 [net.sourceforge.htmlcleaner/htmlcleaner "2.5"]]
+                 [net.sourceforge.htmlcleaner/htmlcleaner "2.6"]]
   :exclusions [org.apache.ant/ant]
   :debug false
   :global-vars {*warn-on-reflection* true
@@ -23,8 +24,12 @@
              :debug
              {:debug true
               :injections [(prn (into {} (System/getProperties)))]}}
+  :aot :all
   :omit-source true
-  :resource-paths ["resources"]
+  :source-paths ["src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :resource-paths ["resources" "src/main/resource"]
+  :test-paths ["src/test/clojure"]
   :jar-exclusions [#"(?:^|/).svn" #"(?:^|/).git" #"(?:\w+).xcf$" #"(?:\w+).pdn$"]
 ;;  :repositories [["sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}]]
   :main nico.core)
