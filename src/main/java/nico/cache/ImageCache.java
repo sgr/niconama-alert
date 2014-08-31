6a9716a57526b1933cfe4d40d55f3bbda31848c0
Shigeru Fujiwara <sgr0502@gmail.com>
2014-08-31 13:08:52 +0900

    changed image scaling algorithm

--- src/main/java/nico/cache/ImageCache.java
+++ src/main/java/nico/cache/ImageCache.java
@@ -255,25 +255,34 @@ public class ImageCache {
 		if ((img != null) &&
 		    (img.getWidth(null) > _width) ||
 		    (img.getHeight(null) > _height)) {
-		    try {
-			BufferedImage scaledImg = new BufferedImage(_width, _height, BufferedImage.TYPE_INT_ARGB);
-			Graphics2D g = scaledImg.createGraphics();
-			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-			g.drawImage(img, 0, 0, _width, _height, null);
-			log.log(Level.FINE,
-				MessageFormat.format("scaled image: ({0}, {1}) -> ({2}, {3})",
-						     img.getWidth(null), img.getHeight(null),
-						     scaledImg.getWidth(null), scaledImg.getHeight(null)));
-			g.dispose();
-			img.flush();
-			return scaledImg;
-		    } catch (Exception e) {
-			log.log(Level.SEVERE,
-				MessageFormat.format("failed scaling image: ({0}, {1})",
-						     img.getWidth(null), img.getHeight(null)),
-				e);
-			return img;
-		    }
+		    // try {
+		    // 	BufferedImage scaledImg = new BufferedImage(_width, _height, BufferedImage.TYPE_INT_ARGB);
+		    // 	Graphics2D g = scaledImg.createGraphics();
+		    // 	g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		    // 	g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		    // 	g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		    // 	g.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+		    // 	g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		    // 	g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		    // 	g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		    // 	g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		    // 	g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
+		    // 	g.drawImage(img, 0, 0, _width, _height, null);
+		    // 	log.log(Level.FINE,
+		    // 		MessageFormat.format("scaled image: ({0}, {1}) -> ({2}, {3})",
+		    // 				     img.getWidth(null), img.getHeight(null),
+		    // 				     scaledImg.getWidth(null), scaledImg.getHeight(null)));
+		    // 	g.dispose();
+		    // 	img.flush();
+		    // 	return scaledImg;
+		    // } catch (Exception e) {
+		    // 	log.log(Level.SEVERE,
+		    // 		MessageFormat.format("failed scaling image: ({0}, {1})",
+		    // 				     img.getWidth(null), img.getHeight(null)),
+		    // 		e);
+		    // 	return img;
+		    // }
+		    return img.getScaledInstance(_width, _height, Image.SCALE_AREA_AVERAGING);
 		} else {
 		    return img;
 		}
