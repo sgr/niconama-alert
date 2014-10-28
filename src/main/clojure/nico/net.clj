497ec4ee3e5809113df00371f42e88a9484dd121
Shigeru Fujiwara <sgr0502@gmail.com>
2014-10-28 11:54:47 +0900

    restricted number of HTTP connections simultaneously

--- src/main/clojure/nico/net.clj
+++ src/main/clojure/nico/net.clj
@@ -2,17 +2,31 @@
 (ns nico.net
   (:require [clojure.tools.logging :as log]
             [clj-http.client :as hc]
-            [clj-http.conn-mgr :as cm]))
+            [clj-http.conn-mgr :as cm])
+  (:import [java.util.concurrent Callable Executors Future TimeUnit]))
 
-(let [cm (cm/make-reusable-conn-manager {:timeout 90 :threads 6})
+(let [nthreads 3
+      cm (cm/make-reusable-conn-manager {:timeout 10 :threads nthreads :default-per-route nthreads})
+      pool (Executors/newFixedThreadPool nthreads)
       HTTP-OPTS  {:headers {"user-agent" "NiconamaAlert.clj"}
                   :connection-manager cm
-                  :conn-timeout   5000
-                  :socket-timeout 90000
+                  :conn-timeout   2000
+                  :socket-timeout 5000
+                  :force-redirects true
                   :follow-redirects true}]
-  (defn http-get [url & opts] (hc/get url (merge HTTP-OPTS (first opts))))
-  (defn http-post [url & opts] (hc/post url (merge HTTP-OPTS (first opts))))
-  (defn shutdown-conn-manager [] (cm/shutdown-manager cm)))
+  (defn http-get [url & opts]
+    (let [^Callable c (cast Callable #(hc/get url (merge HTTP-OPTS (first opts))))
+          ^Future task (.submit pool c)]
+      (.get task)))
+  (defn http-post [url & opts]
+    (let [^Callable c (cast Callable #(hc/post url (merge HTTP-OPTS (first opts))))
+          ^Future task (.submit pool c)]
+      (.get task)))
+  (defn shutdown-conn-manager []
+    (log/info "shutdown HTTP connection manager")
+    (.shutdown pool)
+    (cm/shutdown-manager cm)
+    (.awaitTermination pool 5 TimeUnit/SECONDS)))
 
 (defmacro with-http-res [bindings & body]
   (assert (vector? bindings)     "with-http-res: a vector for its binding")
