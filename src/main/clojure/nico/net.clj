266cf34fbd31b4db2e096e842eaa03ff7f359953
Shigeru Fujiwara <sgr0502@gmail.com>
2014-11-02 00:47:26 +0900

    removed pool from net.clj

--- src/main/clojure/nico/net.clj
+++ src/main/clojure/nico/net.clj
@@ -2,12 +2,10 @@
 (ns nico.net
   (:require [clojure.tools.logging :as log]
             [clj-http.client :as hc]
-            [clj-http.conn-mgr :as cm])
-  (:import [java.util.concurrent Callable Executors Future TimeUnit]))
+            [clj-http.conn-mgr :as cm]))
 
 (let [nthreads 3
       cm (cm/make-reusable-conn-manager {:timeout 10 :threads nthreads :default-per-route nthreads})
-      pool (Executors/newFixedThreadPool nthreads)
       HTTP-OPTS  {:headers {"user-agent" "NiconamaAlert.clj"}
                   :connection-manager cm
                   :conn-timeout   2000
@@ -15,15 +13,9 @@
                   :force-redirects true
                   :follow-redirects true}]
   (defn http-get [url & opts]
-    (let [^Callable c (cast Callable #(hc/get url (merge HTTP-OPTS (first opts))))
-          ^Future task (.submit pool c)]
-      (.get task)))
+    (hc/get url (merge HTTP-OPTS (first opts))))
   (defn http-post [url & opts]
-    (let [^Callable c (cast Callable #(hc/post url (merge HTTP-OPTS (first opts))))
-          ^Future task (.submit pool c)]
-      (.get task)))
+    (hc/post url (merge HTTP-OPTS (first opts))))
   (defn shutdown-conn-manager []
     (log/info "shutdown HTTP connection manager")
-    (.shutdown pool)
-    (cm/shutdown-manager cm)
-    (.awaitTermination pool 5 TimeUnit/SECONDS)))
+    (cm/shutdown-manager cm)))
