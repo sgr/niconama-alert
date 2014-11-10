6f1869514cd9f156cba4da10f342c4910aa6cc1f
Shigeru Fujiwara <sgr0502@gmail.com>
2014-11-11 01:32:27 +0900

    SQLite in-memory DB用のVACUUMを実装

--- src/main/clojure/nico/db.clj
+++ src/main/clojure/nico/db.clj
@@ -14,9 +14,10 @@
 
 ;; DDL ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(def ^{:private true} CONN-URI "jdbc:sqlite::memory:") ;; "jdbc:sqlite:file::memory:?cache=shared"
 (def ^{:private true} CCOL "ccol") ;; concatenated column
 
-(defn- create-db [db]
+(defn- create-db! [db]
   (letfn [(varchar [len] (format "VARCHAR(%d)" len))]
     (jdbc/db-do-commands
      db true
@@ -38,6 +39,16 @@
       [:fetched_at :timestamp]
       [:updated_at :timestamp]
       :table-spec "WITHOUT ROWID"))
+    "CREATE INDEX idx_pgms_id ON pgms(id)"
+    "CREATE INDEX idx_pgms_title ON pgms(title)"
+    "CREATE INDEX idx_pgms_open_time ON pgms(open_time)"
+    "CREATE INDEX idx_pgms_start_time ON pgms(start_time)"
+    "CREATE INDEX idx_pgms_description ON pgms(description)"
+    "CREATE INDEX idx_pgms_category ON pgms(category)"
+    "CREATE INDEX idx_pgms_owner_name ON pgms(owner_name)"
+    "CREATE INDEX idx_pgms_comm_id ON pgms(comm_id)"
+    "CREATE INDEX idx_pgms_comm_name ON pgms(comm_name)"
+    "CREATE INDEX idx_pgms_updated_at ON pgms(updated_at)"
     "PRAGMA journal_mode = OFF"))
 
 ;; Query generator ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -169,9 +180,18 @@
         (log/errorf e "failed add! pgms (%s)" (pr-str (map :id upgms)))
         [0 0]))))
 
-(defn ^String now-str []
-  (let [^FastDateFormat f (FastDateFormat/getInstance "HH:mm:ss")]
-    (.format f (System/currentTimeMillis))))
+(defn- vacuum!
+  "Sqliteのin-memory DBに対するVACUUM代替処理。
+  in-memory DBはVACUUMクエリが無効なようなので(少なくともMac版はできてなさげ)、
+  新しいin-memory DBを作成し今のDBの内容をコピーする、というかなり雑な処理である。
+  なお本プログラムのDBは1テーブルしかなく行数も7000を超えない程度しかない小規模なものであり、
+  実測で繁忙期でも高々1.3秒程度しかかからないことがわかったため、RSS登録サイクルの終了時に毎回実行する。"
+  [db]
+  (let [ndb (assoc db :connection (DriverManager/getConnection CONN-URI))]
+    (create-db! ndb)
+    (jdbc/with-db-transaction [ndb ndb]
+      (jdbc/query db "SELECT * FROM pgms" :row-fn #(jdbc/insert! ndb :pgms %)))
+    ndb))
 
 (defn boot
   "番組情報を保持するDBインスタンスを生成し、コントロールチャネルを返す。
@@ -221,107 +241,83 @@
           (clean! [db target-val]
             (let [q ["id IN (SELECT id FROM pgms WHERE open_time < ? AND updated_at < ? ORDER BY updated_at LIMIT ?)"
                      (timel-before 30) (timel-before 5) target-val]]
-              (jdbc/delete! db :pgms q)))
-          (pragma-query [db k]
-            (jdbc/query db [(str "PRAGMA " (name k))] :result-set-fn first :row-fn k))
-          (now [] (System/currentTimeMillis))]
-    (let [RATIO 1.05
-          CLEAN-INTERVAL 30000
-          SEARCH-INTERVAL 5000
-          SEARCH-LIMIT 50
-          FREELIST-THRESHOLD 10
-          CONN-URI "jdbc:sqlite:file::memory:?cache=shared"
+              (apply + (jdbc/delete! db :pgms q))))]
+    (let [KEEP-RATIO 1.05 ; ニコ生から得られた全数に対してDBの保持数がこの比率を超えたら削除する
+          CLEAN-INTERVAL 30000 ; 古い番組情報を削除間隔ミリ秒
+          SEARCH-INTERVAL 5000 ; 通常検索の実行間隔ミリ秒
+          SEARCH-LIMIT 50 ; オンデマンド検索の検索結果上限
+          ^FastDateFormat fmt (FastDateFormat/getInstance "HH:mm:ss")
           cc (ca/chan)]
       (Class/forName "org.sqlite.JDBC")
-      (ca/go-loop [db {:connection-uri CONN-URI ; これにより都度コネクションが作られる
-                       :reserved-conn (DriverManager/getConnection CONN-URI) ; メモリDB保持のため
+      (ca/go-loop [db {:connection (DriverManager/getConnection CONN-URI) ; メモリDB保持のため
                        :qs (atom {})} ; UIで設定されたクエリー。key: id, value: query string
-                   total 0 ; ニコ生から得た総番組数。cleanタイミングを決めるのに用いる。
-                   npgms 0 ; DBに格納されている総番組数。cleanやvacuumタイミングを決めるのに用いる。
-                   last-cleaned (now) ; 最終削除時刻。削除頻度を上げ過ぎないために用いる。
-                   last-searched (now) ; 最終検索時刻。オンデマンド検索は含まない。検索頻度を上げ過ぎないために用いる。
-                   acc-rm 0] ; 累積削除数。vacuumタイミングを決めるのに用いる。
-
-        (cond
-         (and (> npgms (int (* RATIO total)) 0) ;; 30分経って更新されてない番組情報は削除する
-              (< CLEAN-INTERVAL (- (now) last-cleaned)))
-         (let [threshold (int (* RATIO total))]
-           (clean! db (- npgms threshold))
-           (let [new-npgms (n-pgms db)
-                 last-updated (now-str)
-                 rm (- npgms new-npgms)
-                 new-acc-rm (+ acc-rm rm)
-                 now (now)]
-             (log/infof "clean! [%d -> %d] / %d acc-rm: %d" npgms new-npgms total new-acc-rm)
-             (ca/>! oc-ui {:status :db-stat :npgms new-npgms :last-updated last-updated :total total})
-             (when (pos? rm)
-               (ca/>! oc-ui {:status :searched :results (search-pgms-by-queries db)}))
-             (recur db
-                    total
-                    new-npgms
-                    now
-                    (if (pos? rm) now last-searched)
-                    new-acc-rm)))
-         
-         (and (pos? npgms) (>= acc-rm npgms) ;; acc-rm等を見て必要に応じてvacuumをかける
-              (< FREELIST-THRESHOLD (pragma-query db :freelist_count)))
-         (let [fc (pragma-query db :freelist_count)
-               pc (pragma-query db :page_count)]
-           (jdbc/execute! db ["VACUUM"] :transaction? false)
-           (jdbc/execute! db ["PRAGMA shrink_memory"] :transaction? false)
-           (log/infof "freelist / page: [%d, %d] -> [%d, %d]" fc pc
-                      (pragma-query db :freelist_count) (pragma-query db :page_count))
-           (recur db total npgms last-cleaned last-searched 0))
-
-         :else
-         (if-let [c (ca/<! cc)]
-           (condp = (:cmd c)
-             :create-db (let [now (now)]
-                          (create-db db)
-                          (recur db 0 0 now now 0))
-             :set-query-kwd (let [{:keys [id query target]} c
-                                  q (sql-kwd query target)]
+                   total 0 ; ニコ生から得た総番組数。
+                   npgms 0 ; DBに格納されている総番組数。
+                   last-cleaned  0  ; 最終削除時刻。削除頻度を上げ過ぎないために用いる。
+                   last-searched 0] ; 最終検索時刻。オンデマンド検索は含まない。検索頻度を上げ過ぎないために用いる。
+        (if-let [c (ca/<! cc)]
+          (condp = (:cmd c)
+            :create-db (let [now (System/currentTimeMillis)]
+                         (create-db! db)
+                         (recur db 0 0 now now))
+            :set-query-kwd (let [{:keys [id query target]} c
+                                 q (sql-kwd query target)]
+                             (swap! (:qs db) assoc id q)
+                             (ca/>! oc-ui {:status :searched :results (search-pgms-by-queries db)})
+                             (recur db total npgms last-cleaned last-searched))
+            :set-query-user (let [{:keys [id comms]} c
+                                  q (sql-comms comms)]
                               (swap! (:qs db) assoc id q)
                               (ca/>! oc-ui {:status :searched :results (search-pgms-by-queries db)})
-                              (recur db total npgms last-cleaned last-searched acc-rm))
-             :set-query-user (let [{:keys [id comms]} c
-                                   q (sql-comms comms)]
-                               (swap! (:qs db) assoc id q)
-                               (ca/>! oc-ui {:status :searched :results (search-pgms-by-queries db)})
-                               (recur db total npgms last-cleaned last-searched acc-rm))
-             :rem-query (let [id (:id c)]
-                          (swap! (:qs db) dissoc id)
-                          (recur db total npgms last-cleaned last-searched acc-rm))
-             :add-pgms (let [{:keys [pgms force-search]} c
-                             [ins rm] (if (and pgms (-> pgms count pos?)) (add! db pgms) [0 0])
-                             npgms (n-pgms db)
-                             new-last-searched (let [now (now)]
-                                                 (if (and (< SEARCH-INTERVAL (- now last-searched))
-                                                          (some pos? [ins rm]))
-                                                   now last-searched))]
-                         (ca/>! oc-ui {:status :db-stat :npgms npgms :last-updated (now-str) :total total})
-                         (when (or force-search (not= last-searched new-last-searched))
-                           (ca/>! oc-ui {:status :searched :results (search-pgms-by-queries db)}))
-                         (recur db total npgms last-cleaned new-last-searched (+ acc-rm rm)))
-             :set-total (let [new-total (:total c)]
-                          (when (pos? new-total) (log/infof "SET-TOTAL: %d -> %d" total new-total))
-                          (recur db (if (pos? new-total) new-total total) npgms last-cleaned last-searched acc-rm))
-             :search-ondemand (let [{:keys [query target]} c
-                                    cnt (count-pgms db query target)
-                                    q (if (< SEARCH-LIMIT cnt)
-                                        (sql-kwd query target SEARCH-LIMIT)
-                                        (sql-kwd query target))]
-                                (ca/>! oc-ui {:status :searched-ondemand :cnt cnt :results (search-pgms db q)})
-                                ;; ここではlast-searched更新しない
-                                (recur db total npgms last-cleaned last-searched acc-rm))
-             (do
-               (log/warnf "caught an unknown command[%s]" (pr-str c))
-               (recur db total npgms last-cleaned last-searched acc-rm)))
-           (do
-             (log/info "closed database control channel")
-             (try
-               (.close (:reserved-conn db))
-               (catch Exception e
-                 (log/errorf e "failed closing connection")))))))
+                              (recur db total npgms last-cleaned last-searched))
+            :rem-query (let [id (:id c)]
+                         (swap! (:qs db) dissoc id)
+                         (recur db total npgms last-cleaned last-searched))
+            :add-pgms (let [{:keys [pgms force-search]} c
+                            [ins rm] (if (and pgms (-> pgms count pos?)) (add! db pgms) [0 0])
+                            now (System/currentTimeMillis)
+                            threshold (int (* KEEP-RATIO total))
+                            [new-last-cleaned rm2] (if (and (> npgms threshold 0) (< CLEAN-INTERVAL (- now last-cleaned)))
+                                                     [now (clean! db (- npgms threshold))]
+                                                     [last-cleaned 0])
+                            npgms (n-pgms db)
+                            ndb (if (and (-> pgms count zero?) (pos? npgms))
+                                  (let [a (System/currentTimeMillis)
+                                        ndb (vacuum! db)]
+                                    ;; RSSサイクルの最後は空のpgmsがくるのでVACUUMする
+                                    (log/infof "vacuum DB (%d msec)" (- (System/currentTimeMillis) a))
+                                    (.close (:connection db)) ; 古い方は削除
+                                    ndb)
+                                  db)
+                            new-last-searched (if (or (and (< SEARCH-INTERVAL (- now last-searched)) (some pos? [ins rm]))
+                                                      (not= last-cleaned new-last-cleaned)) 
+                                                now last-searched)]
+                        (ca/>! oc-ui {:status :db-stat :npgms npgms :last-updated (.format fmt (System/currentTimeMillis)) :total total})
+
+                        (when (or force-search (not= last-searched new-last-searched))
+                          (ca/>! oc-ui {:status :searched :results (search-pgms-by-queries ndb)}))
+
+                        (recur ndb total npgms new-last-cleaned new-last-searched))
+            :set-total (let [new-total (:total c)]
+                         (when (pos? new-total) (log/infof "SET-TOTAL: %d -> %d" total new-total))
+                         (recur db (if (pos? new-total) new-total total) npgms last-cleaned last-searched))
+            :search-ondemand (let [{:keys [query target]} c
+                                   cnt (count-pgms db query target)
+                                   q (if (< SEARCH-LIMIT cnt)
+                                       (sql-kwd query target SEARCH-LIMIT)
+                                       (sql-kwd query target))]
+                               (ca/>! oc-ui {:status :searched-ondemand :cnt cnt :results (search-pgms db q)})
+                               ;; ここではlast-searched更新しない
+                               (recur db total npgms last-cleaned last-searched))
+            (do
+              (log/warnf "caught an unknown command[%s]" (pr-str c))
+              (recur db total npgms last-cleaned last-searched)))
+          (do
+            (log/info "closed database control channel")
+            (try
+              (.close (:connection db))
+              (catch Exception e
+                (log/errorf e "failed closing connection"))))))
+
       (ca/>!! cc {:cmd :create-db})
       cc)))
